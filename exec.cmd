ab646158a40b009256b11d361a885124123a24c1
yukenz <yuyun.purniawan@gmail.com>
2025-06-22 00:00:07 +0700

    Add: some command reference

--- exec.cmd
+++ exec.cmd
@@ -4,16 +4,26 @@ forge completions powershell
 forge build
 forge clean
 
+rem #### Profiling
+set FOUNDRY_PROFILE=default
+set FOUNDRY_PROFILE=optimize
+
+rem #### Docs
+forge doc --serve --port 4000
+forge doc --build --out ./documentation
+
+rem #### Testing
 forge inspect ERC20Impl bytecode
 forge coverage --no-match-coverage script
+
+rem #### Check / Setup
 forge config --basic
 forge remappings > remappings.txt
 forge tree
 forge geiger --check --full
 
 rem #### Chain Clone
-forge clone --no-git --chain bsc 0x7862D9B4bE2156B15d54F41ee4EDE2d5b0b455e4 UniswapV3Pool --etherscan-api-key 0000
-
+forge clone --no-git --chain base 0x9b9efa5Efa731EA9Bbb0369E91fA17Abf249CFD4 DexRouter
 
 rem #### Lib Management
 forge update
@@ -48,7 +58,11 @@ rem --constructor-args "My Token" "MT"
 
 rem ####========== Wallet Utils
 cast wallet import index0 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+cast wallet remove --name monad-wallet
+cast wallet list
 cast wallet address --account index0
+cast wallet sign --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 "hello"
+cast wallet verify --address 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 "hello" 0xf16ea9a3478698f695fd1401bfe27e9e4a7e8e3da94aa72b021125e31fa899cc573c48ea3fe1d4ab61a9db10c19032026e3ed2dbccba5a178235ac27f94504311c
 
 rem ####========== Cache
 forge cache ls
@@ -69,7 +83,6 @@ cast nonce 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --rpc-url %LOCALHOST_RPC_U
 
 cast code 0x5FbDB2315678afecb367f032d93F642f64180aa3 --rpc-url %LOCALHOST_RPC_URL%
 cast codesize  0x5FbDB2315678afecb367f032d93F642f64180aa3 --rpc-url %LOCALHOST_RPC_URL%
-
 
 rem ####========== Anvil
 anvil --odyssey --optimism
