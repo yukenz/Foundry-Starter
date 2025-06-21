927ecdd6c4831cca6fa88f90204ddb4111a87ac7
yukenz <yuyun.purniawan@gmail.com>
2025-06-21 19:54:55 +0700

    Add ERC20 Example

---
+++ src/ERC20Impl.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+
+contract CounterScript is ERC20 {
+    constructor() ERC20("We Can", "WCAN") {}
+}
